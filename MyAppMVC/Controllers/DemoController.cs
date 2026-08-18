2baad44b001475f834365895930ef82e2625e663
FMgSi <batrunghoang832@gmail.com>
2026-08-18 22:26:36 +0700

    First commit

---
+++ MyAppMVC/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAppMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return Home();
+        }
+        public IActionResult Introduct()
+        {
+            return Introduct();
+        }
+        public IActionResult Product() { 
+            return Product();
+        }
+        public IActionResult Contact()
+        {
+            return Contact();
+        }
+    }
+}
---
+++ MyAppMVC/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAppMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Product()
+        {
+            return Product();
+        }
+    }
+}
