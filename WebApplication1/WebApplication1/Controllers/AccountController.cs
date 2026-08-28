b8a1a0d47288734ac46e120d88e49abde0ddac2f
FMgSi <batrunghoang832@gmail.com>
2026-08-28 21:24:27 +0700

    Add new folder

---
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Reflection;
+using System.Xml.Linq;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Account> accounts = new List<Account>
+            {
+                new Account() {
+                    Id = 1, Name = "Hoàng Anh",
+                    Email = "anh@gmail.com",
+                    Phone = "0986456789",
+                    Address = "Hà Nội",
+                    Avatar = Url.Content("~/Avatar/02.jfif"),
+                    Gender = 1, Bio = "My name is small",
+                    Birthday = new DateTime(1998, 7, 15)
+                },
+                new Account()
+                {
+                    Id = 1,
+                    Name = "Trường Giang",
+                    Email = "giang@gmail.com",
+                    Phone = "0986456789",
+                    Address = "Hà Nội",
+                    Avatar = Url.Content("~/Avatar/03.jfif"),
+                    Gender = 1,
+                    Bio = "My name is small",
+                    Birthday = new DateTime(1998, 7, 15)
+                },
+                new Account()
+                {
+                    Id = 1,
+                    Name = "Hoàng Thúy",
+                    Email = "thuy@gmail.com",
+                    Phone = "0986456789",
+                    Address = "Hà Nội",
+                    Avatar = Url.Content("~/Avatar/04.jfif"),
+                    Gender = 1,
+                    Bio = "My name is small",
+                    Birthday = new DateTime(1998, 7, 15)
+                },
+            };
+            ViewBag.Accounts = accounts;
+            return View();
+        }
+    }
+}
