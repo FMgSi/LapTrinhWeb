e6cc2750f7f69917b1860cf57ae9f225464bc4e3
FMgSi <batrunghoang832@gmail.com>
2026-09-12 10:58:49 +0700

    add

---
+++ WebApplication3/WebApplication3/ViewComponents/BookViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.ViewComponents
+{
+    public class BookViewComponent : ViewComponent
+    {
+        protected Book book = new Book();
+
+        // Phương thức Invoke sẽ tự động chạy khi component được gọi trên View
+        public IViewComponentResult Invoke()
+        {
+            var books = book.GetBookList();
+            return View(books); // Sẽ tìm file Default.cshtml tương ứng
+        }
+    }
+}
